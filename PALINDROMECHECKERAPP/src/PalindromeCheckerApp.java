3e4950a2d605fd9a612aabc02c79ec9799c5ee68
shivanshshrivastava695 <shivanshshrivastava695@gmail.com>
2026-03-11 15:42:34 +0530

    complted uc10

--- PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
+++ PALINDROMECHECKERAPP/src/PalindromeCheckerApp.java
@@ -1,38 +1,33 @@
 import java.util.Scanner;
 
-import java.util.LinkedList;
-
 public class PalindromeCheckerApp {
     static void main() {
+
 
                 Scanner sc = new Scanner(System.in);
 
                 System.out.print("Enter a string: ");
                 String input = sc.nextLine();
 
-                LinkedList<Character> list = new LinkedList<>();
+                // Convert to lowercase and remove spaces
+                input = input.toLowerCase().replaceAll("\\s+", "");
 
-                // Add characters to LinkedList
-                for (int i = 0; i < input.length(); i++) {
-                    list.add(input.charAt(i));
-                }
+                int start = 0;
+                int end = input.length() - 1;
 
                 boolean isPalindrome = true;
 
-                // Compare first and last characters
-                while (list.size() > 1) {
-                    char first = list.removeFirst();
-                    char last = list.removeLast();
-
-                    if (first != last) {
+                while (start < end) {
+                    if (input.charAt(start) != input.charAt(end)) {
                         isPalindrome = false;
                         break;
                     }
+                    start++;
+                    end--;
                 }
 
-                // Print result
                 if (isPalindrome) {
-                    System.out.println("The given string is a Palindrome");
+                    System.out.println("The given string    is a Palindrome");
                 } else {
                     System.out.println("The given string is NOT a Palindrome");
                 }
